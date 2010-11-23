449a904b485f9ea7ee69b67a4c9b85631fc29878
David Peterson <peterson@orbitec.com.au>
2010-11-24 00:40:07 +1100

    minor fix

--- lib/zeus-api.rb
+++ lib/zeus-api.rb
@@ -1,4 +1,4 @@
-for binding in ["VirtualServer", "Pool"]
+for binding in ["VirtualServer", "Pool", "CatalogRule"]
   for suffix in ["", "Driver", "MappingRegistry", "Service"]
     require File.dirname(__FILE__) + "/api/#{binding.downcase}/#{binding}#{suffix}"
   end
